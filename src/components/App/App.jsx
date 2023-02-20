aac061aed78a45971c74f2426791a110232496d9
AlexeyVorobyov95 <forced1395@gmail.com>
2023-02-20 18:13:05 +0200

    add fix_№3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,17 +9,6 @@ import { TransactionHistory } from '../Transactions/TransactionHistory';
 import { GlobalStyle, Container } from './App.style';
 
 export const App = () => {
- const userData = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308
-  }
-  }
   return (
     <Container>
       <GlobalStyle />
